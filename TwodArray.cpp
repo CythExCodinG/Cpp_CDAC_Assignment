fbd81f126fdbaff97becdd57e312a5cfa0c5d025
CythExCodinG <cytherohit01@gmail.com>
2026-08-27 00:42:50 +0530

    2d array done

--- TwodArray.cpp
+++ TwodArray.cpp
@@ -4,213 +4,206 @@ using namespace std;
 int main()
 {
     int a[10][10];
-    int r, c;
-    int choice;
+    int r,c,choice;
 
-    cout << "Enter number of rows: ";
-    cin >> r;
-    cout << "Enter number of columns: ";
-    cin >> c;
+    cout<<"Enter number of rows : ";
+    cin>>r;
+
+    cout<<"Enter number of columns : ";
+    cin>>c;
 
     do
     {
-        // displaying menu here
-    cout << "\n\n----- MENU -----\n";
-        cout << "1. Accept data\n";
-        cout << "2. Display data\n";
-        cout << "3. Find maximum number\n";
-        cout << "4. Find minimum number\n";
-        cout << "5. Addition of all numbers\n";
-        cout << "6. Sum of each row\n";
-        cout << "7. Sum of each column\n";
-        cout << "8. Row wise maximum\n";
-        cout << "9. Column wise maximum\n";
-        cout << "10. Exit\n";
+        cout<<"\n\n1. Accept data";
+        cout<<"\n2. Display data";
+        cout<<"\n3. Find maximum number";
+        cout<<"\n4. Find minimum number";
+        cout<<"\n5. Find addition of all numbers";
+        cout<<"\n6. Find sum of each row";
+        cout<<"\n7. Find sum of each column";
+        cout<<"\n8. Find row wise maximum";
+        cout<<"\n9. Find column wise maximum";
+        cout<<"\n10. Exit";
 
-        cout << "Enter your choice: ";
-        cin >> choice;
+        cout<<"\nEnter choice : ";
+        cin>>choice;
 
         switch(choice)
         {
             case 1:
-                // taking array values from user
-                cout << "\nEnter array elements:\n";
+                cout<<"\nEnter elements : \n";
 
-                for(int i=0; i<r; i++)
+                for(int i=0;i<r;i++)
                 {
-                    for(int j=0; j<c; j++)
+                    for(int j=0;j<c;j++)
                     {
-                        cin >> a[i][j];
+                        cin>>a[i][j];
                     }
                 }
                 break;
 
             case 2:
-                // print the 2d array
-                cout << "\nArray is:\n";
+                cout<<"\nArray elements are :\n";
 
                 for(int i=0;i<r;i++)
                 {
                     for(int j=0;j<c;j++)
                     {
-                        cout << a[i][j] << " ";
+                        cout<<a[i][j]<<" ";
                     }
-                    cout << endl;
+                    cout<<"\n";
                 }
                 break;
 
             case 3:
             {
-                // finding maximum value
-                int max = a[0][0];
+                int max=a[0][0];
+                int temp=0;       // not really needed but kept
 
                 for(int i=0;i<r;i++)
                 {
                     for(int j=0;j<c;j++)
                     {
-                        if(a[i][j] > max)
+                        if(a[i][j]>max)
                         {
-                            max = a[i][j];
+                            max=a[i][j];
                         }
                     }
                 }
 
-                cout << "Maximum number = " << max;
+                cout<<"\nMaximum number is : "<<max;
                 break;
             }
 
             case 4:
             {
-                // finding minimum number in array
-                int min = a[0][0];
+                int min=a[0][0];
 
                 for(int i=0;i<r;i++)
                 {
                     for(int j=0;j<c;j++)
                     {
-                        if(a[i][j] < min)
+                        if(a[i][j]<min)
                         {
-                            min = a[i][j];
+                            min=a[i][j];
                         }
                     }
                 }
 
-                cout << "Minimum number = " << min;
+                cout<<"\nMinimum number is : "<<min;
                 break;
             }
 
             case 5:
             {
-                // add all elements
-                int sum = 0;
+                int sum=0;
 
                 for(int i=0;i<r;i++)
                 {
                     for(int j=0;j<c;j++)
                     {
-                        sum = sum + a[i][j];
+                        sum=sum+a[i][j];
                     }
                 }
 
-                cout << "Addition of all numbers = " << sum;
+                cout<<"\nAddition = "<<sum;
                 break;
             }
 
             case 6:
             {
-                // calculating sum of every row
-                cout << "\nSum of each row:\n";
+                cout<<"\nSum of rows :\n";
 
                 for(int i=0;i<r;i++)
                 {
-                    int sum = 0;
+                    int sum=0;
+                    int j=0;
 
-                    for(int j=0;j<c;j++)
+                    while(j<c)
                     {
-                        sum = sum + a[i][j];
+                        sum=sum+a[i][j];
+                        j++;
                     }
 
-                    cout << "Row " << i+1 << " = " << sum << endl;
+                    cout<<"Row "<<i+1<<" sum = "<<sum<<"\n";
                 }
+
                 break;
             }
 
             case 7:
             {
-                // sum of each column
-                cout << "\nSum of each column:\n";
+                cout<<"\nSum of columns :\n";
 
                 for(int j=0;j<c;j++)
                 {
-                    int sum = 0;
+                    int sum=0;
 
                     for(int i=0;i<r;i++)
                     {
-                        sum = sum + a[i][j];
+                        sum=sum+a[i][j];
                     }
 
-                    cout << "Column " << j+1 << " = " << sum << endl;
+                    cout<<"Column "<<j+1<<" sum = "<<sum<<endl;
                 }
+
                 break;
             }
 
             case 8:
             {
-                // maximum from each row
-                cout << "\nRow wise maximum:\n";
+                cout<<"\nRow wise maximum :\n";
 
                 for(int i=0;i<r;i++)
                 {
-                    int max = a[i][0];
+                    int max=a[i][0];
 
-                    // check all values of current row
+                    // starting j from 0 would also work
                     for(int j=1;j<c;j++)
                     {
-                        if(a[i][j] > max)
+                        if(a[i][j]>max)
                         {
-                            max = a[i][j];
+                            max=a[i][j];
                         }
                     }
 
-                    cout << "Maximum of Row " << i+1 << " = " << max << endl;
+                    cout<<"Maximum of row "<<i+1<<" = "<<max<<endl;
                 }
+
                 break;
             }
 
             case 9:
             {
-                // finding max value column wise
-                cout << "\nColumn wise maximum:\n";
+                cout<<"\nColumn wise maximum :\n";
 
                 for(int j=0;j<c;j++)
                 {
-                    int max = a[0][j];
+                    int max=a[0][j];
 
-                    // checking elements of column
                     for(int i=1;i<r;i++)
                     {
-                        if(a[i][j] > max)
+                        if(a[i][j]>max)
                         {
-                            max = a[i][j];
+                            max=a[i][j];
                         }
                     }
 
-                    cout << "Maximum of Column " << j+1 << " = " << max << endl;
+                    cout<<"Maximum of column "<<j+1<<" = "<<max<<endl;
                 }
+
                 break;
             }
 
             case 10:
-                // exit the program
-                cout << "Program ended...";
+                cout<<"\nExiting program...";
                 break;
 
             default:
-                // if user enter wrong choice
-                cout << "Wrong choice!";
+                cout<<"\nInvalid choice...";
         }
 
-    } while(choice != 10);
+    }while(choice!=10);
 
     return 0;
 }
